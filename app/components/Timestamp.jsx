9a136088d52e0de0e145d560bc6b1a111e5958aa
bluebird <bluebird23szn@gmail.com>
2024-06-18 12:43:10 +0000

    initial

---
+++ app/components/Timestamp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Timestamp = () => {
+  return (
+    <div className="h-[80%] border text-[8px] border-gray-800 w-[15rem]">
+    hey
+              </div>
+  );
+}
+
+export default Timestamp;
---
+++ app/components/Videostream.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Videostream = () => {
+  return (
+    <div className="h-[63%] border text-[8px] border-gray-800 w-[95%] px-3 flex flex-row justify-between">
+      <div className='w-[30%] bg-black h-full '>
+        hey
+      </div>
+      <div className='w-[70%] bg-red-500 h-full '>
+        heyy
+      </div>
+    </div>
+  );
+}
+
+export default Videostream;
